8cb64d8bd1713cd0d6c30f0dd6cd63813bfc1d5b
Jacob Good <jacobegood@gmail.com>
2022-09-25 23:26:03 -0400

    first commit

---
+++ base/views.py
@@ -0,0 +1,90 @@
+# from asyncio import Task, tasks
+# from audioop import reverse
+# from operator import truediv
+from re import search
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+from .models import Task
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+
+        return super(RegisterPage, self).form_valid(form)
+
+    # redirect user from register page to tasks page if alread logged in
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated: 
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+# Created the views here.
+class TaskList(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+    # user can only get their own data
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count
+
+        # search all the tasks
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+        
+        context['search_input'] = search_input
+        
+        return context
+    
+class TaskDetail(LoginRequiredMixin, DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    # don't need all feilds
+    # fields = '__all__'
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model = Task
+    # fields = '__all__'
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
